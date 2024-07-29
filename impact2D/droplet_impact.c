834665d735166565b2c1be27405f2b9d75e34c59
Waid Daniel Dunlop <wdunlop3@login-phoenix-slurm-2.pace.gatech.edu>
2024-07-29 19:22:45 -0400

    Reformat simulation to output png images and format outputs

--- impact2D/droplet_impact.c
+++ impact2D/droplet_impact.c
@@ -1,16 +1,21 @@
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <math.h>
+#include <stdlib.h>
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
 #include "tension.h"
 #include "tag.h"
 #include "view.h"
 
+// define constants
+#define MAX_DIGITS 8
 
 // define physical constants and window size
 double drop_dia = 1e-3; // m
 double SIGMA = 72.8e-3; //N/m
 double box_length = 30e-3; // m
-double start_height = 1e-3; //m
+double start_height = 5e-3; //m
 
 // Define relevant dimensionless numbers
 double Re;
@@ -20,26 +25,38 @@ double We;
 scalar f0[];
 
 // define max refinement level for grid
-int MAXLEVEL = 10;
+int MAXLEVEL = 8;
 
 // define error tolerance
 double uemax = 0.1;
 
 // define initial velocity
-double u0 = 3.; // m/s
+double u0 = 1.0; // m/s
+
+// define end time for the simulation
+double t_end = 0.03; 
 
 // boundary conditions
 u.t[bottom] = dirichlet(0.);
 u.n[bottom] = dirichlet(0.);
 f[bottom] = 0;
 
+// Create a name for the directory to save the image files
+char path_str[20];
+
+// define a name for the logfile and a pointer to the logfile
+char logfile[30];
+FILE *logfptr;
+
+// define a name for the output directory which will be created
+char dirname[30];
 
 int main(){
     init_grid(64);
     origin(-box_length/2, 0);
 
     size(box_length);
-
+    
     // define the densities and viscosities of the two fluids
         // fluid 1: Water at room temp
         // fluid 2: Air at room temp
@@ -47,6 +64,15 @@ int main(){
     rho2 = 1.293; // kg/m^3
     mu1 = 0.89e-3; // Pa*s
     mu2 = 1.8e-5; // Pa*s
+     
+    // create a folder for saving the outputs
+    sprintf(dirname, "v=%g__D=%g", u0, drop_dia);
+    mkdir(dirname, 0755);
+
+    // Create the filename and create the file for logging the simulation progress
+    sprintf(logfile, "%s/log.log", dirname);
+    logfptr = fopen(logfile, "w");
+    fclose(logfptr);
 
     // define the surface tension coefficient
     f.sigma = SIGMA;
@@ -81,12 +107,23 @@ event init(t=0){
 
 event log_status(i++){
     // print out the iteration number and time 
-    printf("%d %g\n", i, t);
+    char logstr[20];
+    sprintf(logstr, "%d %g\n", i, t);
+
+    logfptr = fopen(logfile, "a");
+    fputs(logstr, logfptr);
+    fclose(logfptr);
 }
 
-event end(t=0.02){
+event end(t=t_end){
     // Print the Reynold's number and Weber number for this test case
     printf("Re: %d, We:%d\n", (int) round(Re), (int) round(We));
+    char donestr[20];
+    sprintf(donestr, "Re:%d, We:%d\n", (int) round(Re), (int) round(We));
+ 
+    logfptr = fopen(logfile, "a");
+    fputs(donestr, logfptr);
+    fclose(logfptr);
 }
 
 #if 0
@@ -106,17 +143,35 @@ event initial_graphics_display(i=0){
 }
 #endif
 
-event movie(t=0.005; t<=0.014; t+=0.0001){
+event movie(t+=0.0001){
     view(tx=0, ty=-0.5, width=800, height=800);
     clear();
     draw_vof("f");
     squares("u.y", linear=true, spread=10);
     box();
+        
+    char img_index[MAX_DIGITS] = "00000000";
+    
     char filename[50];
-    sprintf(filename, "output_vids/Re=%d_We=%d.mp4", (int) round(Re), (int) round(We));
-    save(filename);
+    if(i != 0){
+       
+        int num_digits = floor(log10(i)) + 1;
+        printf("%d\n", num_digits);
+        char num_as_str[num_digits];
+        sprintf(num_as_str, "%d", i);
+    
+        int start_ind = MAX_DIGITS - num_digits;
+        for(int j=0; j<=num_digits-1;j++){
+            img_index[start_ind + j] = num_as_str[j];
+        }
+    }
+    //sprintf(filename, "output_vids/Re=%d_We=%d.mp4", (int) round(Re), (int) round(We));
+    char png_save_path[50];
+    sprintf(png_save_path, "%s/%s_t=%g.png", dirname, img_index, t);
+    save(png_save_path);
 }
 
 event adapt(i++){
     adapt_wavelet({f, p, u}, (double[]){0.01,0.01,0.01,0.01,0.01}, maxlevel=MAXLEVEL);
 }
+
